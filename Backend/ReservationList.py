c89e551caeb4417f32cc3ca3007a7eb930038de0
Coder109 <3211697682@qq.com>
2024-06-19 16:02:55 +0800

    Update Reservation List Format

--- Backend/ReservationList.py
+++ Backend/ReservationList.py
@@ -9,7 +9,9 @@ class ReservationList():
                  r_time=None,
                  r_place=None,
                  r_content=None,
-                 r_email=None):
+                 r_email=None,
+                 r_id = None,
+                 r_status = None):
         """
         预约信息
         Args:
@@ -17,14 +19,21 @@ class ReservationList():
             r_place: 预约地点
             r_content: 预约内容
             r_email: 预约邮箱
+            r_id: 预约编号
+            r_status: 预约状态
         """
-        if r_time is None or r_place is None or r_content is None or r_email is None:
+        if r_time is None or r_place is None or r_content is None or r_email is None or r_id is None or r_status is None:
             print("[Error]提供给预约的参数不全")
+            return
+        if r_id < 0 or r_id > 4:
+            print("[Error]预约编号不合法")
             return
         self.r_time = r_time
         self.r_place = r_place
         self.r_content = r_content
         self.r_email = r_email
+        self.r_id = r_id
+        self.r_status = r_status
     
     def to_dict(self):
         """
@@ -34,5 +43,7 @@ class ReservationList():
             "time": self.r_time,
             "place": self.r_place,
             "content": self.r_content,
-            "email": self.r_email
+            "email": self.r_email,
+            "id": self.r_id,
+            "status": self.r_status
         }
